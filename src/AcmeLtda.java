257b9464252241a2ed431da07d28016f15d9c101
Hebert Magalhães Soares <hebertms2009@hotmail.com>
2023-05-11 23:09:24 -0300

    Final
    App Final

--- src/AcmeLtda.java
+++ src/AcmeLtda.java
@@ -18,12 +18,12 @@ public class AcmeLtda {
             double QuantidadeIdeal = Math.ceil(peso * 35 / 1000);
             double FaltaIngerir = Math.ceil(QuantidadeIdeal - AguaIngeridaHoje);
 
-            System.out.println(String.format("%s\nVocê pesa: %.0fKg\nHoje você ja ingeriu: %.0f Litros de água\nA quantidade ideal de água para você beber por dia é: %.0f Litros", nome, peso, AguaIngeridaHoje, QuantidadeIdeal));
+            System.out.println(String.format("%s\nVocê pesa: %.0fKg\nHoje você ja ingeriu: %.0fL de água\nA quantidade ideal de água para você beber por dia é: %.0fL", nome, peso, AguaIngeridaHoje, QuantidadeIdeal));
 
         if (FaltaIngerir<=0) {
             System.out.println("Parabéns, você atingiu a meta de hidratação diária");
         } else {
-            System.out.println(String.format("Continue focado em se hidratar por hoje, ainda faltam: %.0f litros! Você consegue!",FaltaIngerir));
+            System.out.println(String.format("Continue focado em se hidratar por hoje, ainda faltam: %.0fL! Você consegue!",FaltaIngerir));
         }
         
     }
